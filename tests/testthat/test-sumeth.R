c5092d6acd0bc33308624497328ff05d60736def
ialfarone <irene.alfarone@gmail.com>
2025-08-05 16:50:43 +0200

    add test_that sumeth

--- tests/testthat/test-sumeth.R
+++ tests/testthat/test-sumeth.R
@@ -1,3 +1,26 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("sumeth works as expected", {
+
+  res <- data.frame(
+    eff1 = c(3.7, 3.5),
+    eff2 = c(3.2, 3.2),
+    se1 = c(1.5, 1.3),
+    se2 = c(2.7, 1.8),
+    cov12 = c(0.02, 0.5)
+  )
+
+  result <- sumeth(
+    eff1 = res$eff1,
+    eff2 = res$eff2,
+    se1 = res$se1,
+    se2 = res$se2,
+    cov12 = res$cov12,
+    method = "Normal (1, 6)"
+  )
+
+  expect_s3_class(result, "data.frame")
+
+  expect_true(all(c("eff1", "eff2", "ci1_lb", "ci1_ub",
+                    "ci2_lb", "ci2_ub") %in% colnames(result)))
+  expect_equal(nrow(result), 1)
+
 })
